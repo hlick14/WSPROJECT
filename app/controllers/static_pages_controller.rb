6c20ad4bddad37d3ee6f3974dfe6473ff7918298
Jakub Pieczonka <hlick14@gmail.com>
2015-11-26 16:10:17 +0000

    WSP Project - 2015

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,16 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+
+   def about      # NEW
+   	#flash[:notice] = "Testing the flash"
+  end
+
+
+  def contact
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+ Rails.application.routes.draw do
+  
+
+ 		  resources :users 
+          root 'static_pages#home'
+
+          get 'help'    => 'static_pages#help'
+          get 'about'   => 'static_pages#about'
+          get 'contact' => 'static_pages#contact'
+          get 'signup'  => 'users#new'
+
+          get    'login'   => 'sessions#new'
+      	  post   'login'   => 'sessions#create'
+          delete 'logout'  => 'sessions#destroy'
+
+
+  
+  
+end
