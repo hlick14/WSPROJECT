cea4ac84ecbb3ad36e34c9a8b77798d0c4f307e6
Jakub Pieczonka <hlick14@gmail.com>
2015-12-06 18:24:28 +0000

    -

--- db/migrate/20151205100458_add_order_to_user.rb
+++ db/migrate/20151205100458_add_order_to_user.rb
@@ -1,5 +1,5 @@
 class AddOrderToUser < ActiveRecord::Migration
   def change
-    add_reference :users, :order, index: true, foreign_key: true
+    add_reference :users, :orders, index: true, foreign_key: true
   end
 end
